1f6d775b6f6db36ff70b642810f61a6bf6114f52
zeubkiller <george@guy.com>
2017-03-18 13:05:22 +0100

    Add gallery content and fake img

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+import {Http} from '@angular/http';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Coucou amandine!';
+  
+  spaceScreens : Array<any>;
+
+  constructor(private http:Http) {
+
+    this.http.get('./src/data.json')
+    .map(response => response.json().screenshots)
+    .subscribe(res => this.spaceScreens = res);
+  }
+
+
 }
