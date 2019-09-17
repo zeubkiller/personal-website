2804a21b57c15bd11b615bf007f79ed8f2f94381
abelard.sylvain@gmail.com <abelard.sylvain@gmail.com>
2019-09-17 16:53:26 +0200

    Create the first index.html template and try to use it in the server

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask("DRU")
 
 @app.route("/")
+@app.route("/index")
 def hello():
-    return "Hello, World!"
+    user = {"username" : "George"}
+    return render_template("index.html", title="Home", user=user)
