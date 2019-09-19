6f4348e9e151ae1dee6257e559917633f0b214a4
abelard.sylvain@gmail.com <abelard.sylvain@gmail.com>
2019-09-19 19:55:05 +0200

    The template is working correctly

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
 
 app = Flask("DRU")
+app.config['EXPLAIN_TEMPLATE_LOADING']=True
 
 @app.route("/")
 @app.route("/index")
 def hello():
     user = {"username" : "George"}
-    return render_template("index.html", title="Home", user=user)
+    return render_template('index.html', title="Home", user=user)
+
