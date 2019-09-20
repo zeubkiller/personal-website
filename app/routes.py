3b7135cff198d29ab8f177300240c106db4a54f5
abelard.sylvain@gmail.com <abelard.sylvain@gmail.com>
2019-09-20 15:10:01 +0200

    Create a login page for user

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,15 @@
-from app import serverApp
 from flask import render_template
+
+from app import serverApp
+from app.forms import LoginForm
 
 @serverApp.route("/")
 @serverApp.route("/index")
 def hello():
     user = {"username" : "George"}
     return render_template('index.html', title="Home", user=user)
+
+@serverApp.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
