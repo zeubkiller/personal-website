3092774512fe9a36bfb021aeb770c33bef69aece
abelard.sylvain@gmail.com <abelard.sylvain@gmail.com>
2019-09-20 17:26:32 +0200

    Add a function to make the flask shell commande easy to use for testing

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 
-from app import serverApp
+from app import serverApp, database_instance
+from app.models import User, Post
+
+@serverApp.shell_context_processor
+def make_shell_context():
+    return {'db' : database_instance, 'User': User, 'Post': Post}
