523c97501d2f550705f8584b29c25914187be8c7
abelard.sylvain@gmail.com <abelard.sylvain@gmail.com>
2019-09-20 17:13:58 +0200

    Create a new table called with the post of each users

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from app import database_instance
 
 class User(database_instance.Model):
@@ -5,6 +7,16 @@ class User(database_instance.Model):
     username = database_instance.Column(database_instance.String(64), index=True, unique=True)
     email = database_instance.Column(database_instance.String(120), index=True, unique=True)
     password_hash = database_instance.Column(database_instance.String(128), index=True, unique=True)
+    posts = database_instance.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<user>{}'.format(self.username)
+
+class Post(database_instance.Model):
+    id = database_instance.Column(database_instance.Integer, primary_key=True)
+    body = database_instance.Column(database_instance.String(140), index=True)
+    timestamp = database_instance.Column(database_instance.DateTime, index=True, default=datetime.utcnow)
+    user_id = database_instance.Column(database_instance.Integer, database_instance.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
