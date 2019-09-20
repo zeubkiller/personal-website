f3fb1b497b03265db102087731571f2ae06bd81c
abelard.sylvain@gmail.com <abelard.sylvain@gmail.com>
2019-09-20 16:44:41 +0200

    Prepare the installation of the database for the server

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 from app.configurations import ServerConfiguration
 
 serverApp = Flask(__name__)
 serverApp.config.from_object(ServerConfiguration)
 
+database_instance = SQLAlchemy(serverApp)
+migrate = Migrate(serverApp, database_instance)
 
-from app import routes
+from app import routes, models
