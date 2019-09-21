d22cea899c62aec16652b4fd07c234f481545265
abelard.sylvain@gmail.com <abelard.sylvain@gmail.com>
2019-09-21 10:29:34 +0200

    Add a way for the user to log out

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, url_for
 
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, logout_user
 
 from app import serverApp
 from app.forms import LoginForm
@@ -18,7 +18,7 @@ def login():
     
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(user_name=form.username.data).first
+        user = User.query.filter_by(user_name=form.username.data).first()
         if user is None or not user.is_authenticated(form.data.password)
             flash("Invalid username or password")
             return redirect(url_for("login"))
@@ -26,3 +26,8 @@ def login():
         login_user(user, remember=form.remember_me.data)
         redirect(url_for("index"))
     return render_template('login.html', title='Sign In', form=form)
+
+    @serverApp.route("/logout")
+    def logout():
+        logout_user()
+        redirect(url_for("index"))
