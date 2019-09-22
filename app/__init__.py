3da3f5c71e503db6d45bd7402cdb3580641eb900
abelard.sylvain@gmail.com <abelard.sylvain@gmail.com>
2019-09-22 16:49:06 +0200

    create a blueprint for the authentification

--- app/__init__.py
+++ app/__init__.py
@@ -14,25 +14,15 @@ from app.configurations import ServerConfiguration
 serverApp = Flask(__name__)
 serverApp.config.from_object(ServerConfiguration)
 
-if not serverApp.debug:
-    if not os.path.exists("logs"):
-        os.mkdir("logs")
-    file_handler = RotatingFileHandler("logs/app.log", maxBytes=10240, backupCount=10)
-
-    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-    file_handler.setLevel(logging.INFO)
-
-    serverApp.logger.addHandler(file_handler)
-
-    serverApp.logger.setLevel(logging.INFO)
-    serverApp.logger.info("App startup")
-
 database_instance = SQLAlchemy(serverApp)
 migrate_instance = Migrate(serverApp, database_instance)
 
 login = LoginManager(serverApp)
-login.login_view = "login"
+login.login_view = "authentification.login"
 
 bootstrap = Bootstrap(serverApp)
+
+from app.authentification import auth_bp
+serverApp.register_blueprint(auth_bp)
 
 from app import routes, models, errors
